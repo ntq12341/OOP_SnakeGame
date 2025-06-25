b37348bce10ddc7fca5e1994abb4bd35548deb54
ntq12341 <ntquang227031@gmail.com>
2025-06-26 04:27:27 +0700

    ok

--- WinFormsApp3/obj/Debug/net8.0-windows/WinFormsApp3.AssemblyInfo.cs
+++ WinFormsApp3/obj/Debug/net8.0-windows/WinFormsApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4e7d908b3543362d831fac6cfe6d8065da0c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
